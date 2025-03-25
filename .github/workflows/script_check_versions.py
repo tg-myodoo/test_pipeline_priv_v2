253b7cb44d51b8a9d76969c4b427cd7500e0cf47
tg-myodoo <114913689+tg-myodoo@users.noreply.github.com>
2025-03-25 15:06:41 +0100

    Update script_check_versions.py

--- .github/workflows/script_check_versions.py
+++ .github/workflows/script_check_versions.py
@@ -3,3 +3,14 @@ import os
 print("Jest GIT!")
 
 print(os.getenv('FAKE_TOKEN'))
+
+FAKE_TOKEN = os.environ['FAKE_TOKEN']
+THE_GITHUB_TOKEN = os.environ['THE_GITHUB_TOKEN']
+URL_PULL = os.environ['URL_PULL']
+REPOSITORY = os.environ['REPOSITORY']
+COMPARASION_BASE = os.environ['COMPARASION_BASE']
+
+print(FAKE_TOKEN)
+print(URL_PULL)
+print(REPOSITORY)
+print(COMPARASION_BASE)
