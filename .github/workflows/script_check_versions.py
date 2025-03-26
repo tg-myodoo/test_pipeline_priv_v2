7d5d967f5f0c4454534eb2c95e23b00a967a8d9e
tg-myodoo <114913689+tg-myodoo@users.noreply.github.com>
2025-03-26 11:51:54 +0100

    Update script_check_versions.py

--- .github/workflows/script_check_versions.py
+++ .github/workflows/script_check_versions.py
@@ -15,4 +15,13 @@ print(URL_PULL)
 print(REPOSITORY)
 print(COMPARASION_BASE)
 
-print(os.listdir("/"))
+print("_", os.listdir("/"))
+# ['home', 'sys', 'lib64', 'proc', 'dev', 'lib', 'var', 'etc', 'srv', 'root', 'boot', 'run', 'opt', 'snap', 'mnt', 
+# 'imagegeneration', 'usr', 'media', 'sbin.usr-is-merged', 'lib.usr-is-merged', 'tmp', 'lib32', 'bin.usr-is-merged', 'data', 'sbin', 'bin', 'lost+found']
+print("home", os.listdir("/home"))
+print("sys", os.listdir("/sys"))
+print("run", os.listdir("/run"))
+print("usr", os.listdir("/usr"))
+print("tmp", os.listdir("/tmp"))
+print("data", os.listdir("/data"))
+print("bin", os.listdir("/bin"))
