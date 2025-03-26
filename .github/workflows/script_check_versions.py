559fce72f4184eab8380d255d21f861f504402e8
tg-myodoo <114913689+tg-myodoo@users.noreply.github.com>
2025-03-26 11:58:25 +0100

    Update script_check_versions.py

--- .github/workflows/script_check_versions.py
+++ .github/workflows/script_check_versions.py
@@ -10,6 +10,8 @@ URL_PULL = os.environ['URL_PULL']
 REPOSITORY = os.environ['REPOSITORY']
 COMPARASION_BASE = os.environ['COMPARASION_BASE']
 
+GITHUB_WORKSPACE = os.environ['GITHUB_WORKSPACE']
+
 print(FAKE_TOKEN)
 print(URL_PULL)
 print(REPOSITORY)
@@ -18,10 +20,5 @@ print(COMPARASION_BASE)
 print("_", os.listdir("/"))
 # ['home', 'sys', 'lib64', 'proc', 'dev', 'lib', 'var', 'etc', 'srv', 'root', 'boot', 'run', 'opt', 'snap', 'mnt', 
 # 'imagegeneration', 'usr', 'media', 'sbin.usr-is-merged', 'lib.usr-is-merged', 'tmp', 'lib32', 'bin.usr-is-merged', 'data', 'sbin', 'bin', 'lost+found']
-print("home", os.listdir("/home"))
-print("sys", os.listdir("/sys"))
-print("run", os.listdir("/run"))
-print("usr", os.listdir("/usr"))
-print("tmp", os.listdir("/tmp"))
-print("data", os.listdir("/data"))
-print("bin", os.listdir("/bin"))
+print("GITHUB_WORKSPACE", os.listdir(GITHUB_WORKSPACE))
+
